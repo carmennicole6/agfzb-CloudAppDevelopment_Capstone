23827e1dabd121e9199f85ab47ff514d6fa1e3f5
carmennicole6 <carmen.nicole6@gmail.com>
2023-05-01 08:06:50 -0400

    changes made from theia lab environment

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -3,20 +3,20 @@ from django.conf.urls.static import static
 from django.conf import settings
 from . import views
 
-app_name = 'djangoapp'
+app_name = 'djangoapp' 
 urlpatterns = [
     # route is a string contains a URL pattern
     # view refers to the view function
     # name the URL
 
-    path(route='', view=views.get_dealerships, name='index'),
-
-    path(route="contact/", view=views.contact, name="contact"),
-
-    path(route="about/", view=views.about, name="about"),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('registration/', views.registration_request, name='registration'),
     path('login/', views.login_request, name='login'),
     path('logout/', views.logout_request, name='logout'),
-  # path for dealer reviews view
+    path('dealer/<int:id>/', views.get_dealer_details, name='dealer_details'),
+    path('dealer/<int:id>/review', views.add_review, name='add_review'),
+    path(route='', view=views.get_dealerships, name='index')
+ 
 
-    # path for add a review vi
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
